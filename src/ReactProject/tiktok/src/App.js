356f08c7b20ef42eb345fa217ac90cefd3671f47
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-08-20 10:57:45 +0700

    two-way binding with input tag

--- src/ReactProject/tiktok/src/App.js
+++ src/ReactProject/tiktok/src/App.js
@@ -1,22 +1,15 @@
 
 import { useState } from "react";
-
-let price  = [1, 2, 3]
-
 function App() {
-  
-  const [counter, setCounter] = useState(
-    price.reduce((total, currentValue) => {
-      return total += currentValue;
-    })
-  );
-  const handleIncrease = () =>{
-    setCounter(counter + 1)
-  }
+  const [name, setName] = useState('33');
   return (
     <div className="App" style={{padding: 20}}>
-      <h1>{ counter }</h1>
-      <button onClick={handleIncrease}>Increase</button>
+      <input
+      type="number"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={()=>setName('0')}>Change</button>
     </div>
   );
 }
