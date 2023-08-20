3df5c012a39ac87f3297578f56ccb5bde11b9e24
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-08-20 12:13:52 +0700

    two-way binding with checkbox

--- src/ReactProject/tiktok/src/App.js
+++ src/ReactProject/tiktok/src/App.js
@@ -17,19 +17,30 @@ const courses = [
 ]
 
 function App() {
-  const [checked, setChecked] = useState(2);
-  console.log(checked)
+  const [checked, setChecked] = useState([]);
   const handleSubmit = () => {
-    console.log({id:checked});
+    //Call API
+    console.log({ids:checked});
+  }
+  const handleCheck = (id) =>{
+    setChecked(prev =>{
+      const isChecked = checked.includes(id);
+      if(isChecked){
+        return checked.filter(item =>item !==id)
+      }else {
+        return [...prev, id];
+      }
+      
+    })
   }
   return (
     <div className="App" style={{padding: 20}}>
       {courses.map(course => (
         <div key={course.id}>
           <input 
-            type="radio"
-            checked={checked===course.id}
-            onChange={() => setChecked(course.id)}
+            type="checkbox"
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
           /> 
           {course.name}
         </div>
