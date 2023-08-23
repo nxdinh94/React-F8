68f2f68f821aa58f7fcd34f42d0fa1f6a3b5bed3
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-08-23 14:25:36 +0700

    useMeno()

---
+++ src/ReactProject/tiktok/Theory_Hook/useMemo().md
@@ -0,0 +1,3 @@
+cách hoạt động của tham số giống useEffect()
+
+tránh lặp lại 1 đoạn logic khi không cần thiết
--- src/ReactProject/tiktok/src/App.js
+++ src/ReactProject/tiktok/src/App.js
@@ -1,20 +1,54 @@
 
-import { useCallback, useState } from 'react';
-import Content from './Content';
-
+import { useState, useMemo, memo, useRef } from "react";
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [products, setProducts] = useState([]);
 
-  const handleIncrease =useCallback(() =>{
-    setCount(prevCount => prevCount +1)
-  }, []) 
+  const nameRef = useRef();
+
+  const handleSubmit = () =>{
+    setProducts([
+      ...products, {
+      name: name, 
+      price: Number.parseInt(price)
+    }])
+    setName('')
+    setPrice('')
+    nameRef.current.focus();
+  };
+
+  const total = useMemo(()=>{
+    const result = products.reduce((result, prod) => {
+      return result + prod.price
+    }, 0)
+    return result;
+  }, [products])
 
   return (
     <div style={{padding: '10px 32px'}}>
-      <Content handle = {handleIncrease}/>
-      <h1>{count}</h1>
+      <input 
+        ref={nameRef}
+        value={name}
+        placeholder="Enter name..."
+        onChange={e => setName(e.target.value)}
+      />
+      <br/>
       
-      
+      <input 
+        value={price}
+        placeholder="Enter price..."
+        onChange={e => setPrice(e.target.value)}
+      />
+      <br/>
+      <button onClick={handleSubmit}>Add</button>
+      <br/>
+      Total:{total}
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
