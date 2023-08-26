eeaa0ae69b0879ed0e42235fbcd9fdb9ebd8bdbb
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-08-26 09:03:54 +0700

    router-react-dom

--- src/ReactProject/tiktok/src/App.js
+++ src/ReactProject/tiktok/src/App.js
@@ -1,15 +1,36 @@
-import Button from './components/Button'
-import GlobalStyles from './components/GlobalStyles';
+import {Routes, Route, Link} from 'react-router-dom'
+import HomePage from './pages/Home'
+import NewsPage from './pages/News'
+import ContactPage from './pages/Contact'
+import Home from './pages/Home'
+
 
 function App() {
   return (
-    <GlobalStyles>
-      <div style={{padding: '0 32px'}}>
+    
+      <div className="app" style={{padding: '0 32px'}}>
         
-        <Button/>
-        <Button primary disabled/>{/* return bolean */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/news">News</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/news' element={<NewsPage />}></Route>
+          <Route path='/contact' element={<ContactPage />}></Route>
+        </Routes>
       </div>
-    </GlobalStyles>
+
   )
 }
 
--- src/ReactProject/tiktok/src/index.js
+++ src/ReactProject/tiktok/src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//Fake Comment
-function emitcomment(id){
-  setInterval(()=>{
-    window.dispatchEvent(
-      new CustomEvent(`lesson-${id}`,{detail: `The Comment's content of lessson-${id}`})
-  )}, 2000)}
-
-  emitcomment(1);
-  emitcomment(2);
-  emitcomment(3);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
